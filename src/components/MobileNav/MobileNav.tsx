938037a8454278be6324fd7fe47d7320f1587f25
YashNaj <yashalnajeeb@gmail.com>
2022-10-05 13:19:02 -0700

    modified:   .gitignore 	modified:   src/components/MobileNav/MobileNav.tsx 	modified:   src/containers/about/About.css 	new file:   src/images/fazl.jpg "Added Fazl Picture"

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -16,7 +16,7 @@ const sidebar = {
   closed: {
     clipPath: "circle(0px at 40px 40px)",
     transition: {
-      delay: 0.5,
+      delay: 0.2,
       type: "spring",
       stiffness: 400,
       damping: 40,

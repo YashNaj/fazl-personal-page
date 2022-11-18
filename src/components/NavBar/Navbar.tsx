f4047ca9afae664dd0e3cbbc83e019f789ffd80c
YashNaj <yashalnajeeb@gmail.com>
2022-11-17 17:56:23 -0800

    Added in dynamically underlining navmenu:

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import { MobileNav } from "../MobileNav/MobileNav";
+import { AnimateSharedLayout, motion } from "framer-motion";
+
+const menuItems = ["Work", "About", "Resume"];
+
+const MenuItem = ({ text, selected, onClick }: any) => (
+  <motion.div className = 'Nav__link'
+    onClick={onClick}
+    animate={{ opacity: selected ? 1 : 0.5 }}
+  >
+    <NavLink className = "Nav__link" to={text}>{text}</NavLink>
+    {selected && <motion.div className="underline Nav__link" layoutId="underline" />}
+  </motion.div>
+);
 
 export const NavBar = () => {
+  const [selected, setSelected] = useState(0);
   return (
     <nav className="Nav">
       <div className="Nav__container">
@@ -11,26 +26,17 @@ export const NavBar = () => {
           Fazl Mojadeddi
         </NavLink>
         <div className="Nav__right">
-          <ul className="Nav__item-wrapper">
-            <li className="Nav__item">
-              <NavLink className="Nav__link" to="/work">
-                Work
-              </NavLink>
-            </li>
-            <li className="Nav__item">
-              <NavLink className="Nav__link" to="/about">
-                About
-              </NavLink>
-            </li>
-            <li className="Nav__item">
-              <NavLink className="Nav__link" to="/resume">
-                Resume
-              </NavLink>
-            </li>
-          </ul>
+            {menuItems.map((el, i) => (
+              <MenuItem
+                text={el}
+                key={i}
+                selected={selected === i}
+                onClick={() => setSelected(i)}
+              />
+            ))}
         </div>
         <div className="Nav__container-mobile">
-        <MobileNav/>
+          <MobileNav />
         </div>
       </div>
     </nav>
